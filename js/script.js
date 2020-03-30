e5e14c4f259c174c0094472fc95f8349f895f71e
William He <contact@williamhe.dev>
2020-03-30 11:44:29 -0700

    Started on js

---
+++ js/script.js
@@ -0,0 +1,34 @@
+//======================//
+// Constants            //
+//======================//
+
+/** Maximum number of tries. */
+const MAX_TRIES = 7;
+/** Array of words to guess. */
+const WORDS = [
+    "COMMITTEE", 
+    "Word 2",
+    "Word 3",
+    "Word 4",
+    "Word 5",
+    "Word 6",
+    "Word 7",
+    "Word 8",
+    "Word 9",
+    "Word 10"];
+/** Parallel array to WORDS for their definitions. */
+const DEFS = [
+    "A group of people appointed for a specific function, typically consisting of members of a larger group", 
+    "Definition 2", 
+    "Definition 4",
+    "Definition 5",
+    "Definition 6",
+    "Definition 7",
+    "Definition 8",
+    "Definition 9",
+    "Definition 10"];
+
+//======================//
+// Functions            //
+//======================//
+
